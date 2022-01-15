42924fe775e381e5e7769116d95d448094b7a2cd
rubensabimael <rubensabimael26@gmail.com>
2022-01-15 15:29:21 -0300

    first commit

---
+++ src/CursoDeJava.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+public class CursoDeJava  {
+
+    public static void main (String[] args){
+        sum( 10, 20);
+    }
+
+    static void sum(int x, int  y) {
+
+        System.out.println(x + y);
+    }
+}
